15a133ecced2765beb4cfd86df1b710d0a6145e9
Piotr Gajowniczek <piotrgajow@gmail.com>
2018-11-03 14:37:47 +0100

    Add database support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { UsersService } from './users/users.service';
 
+const dataSourceConfiguration = {
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: '',
+    database: 'job_offers',
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: true,
+} as TypeOrmModuleOptions;
+
 @Module({
     imports: [
         UsersModule,
+        TypeOrmModule.forRoot(dataSourceConfiguration),
     ],
     controllers: [
         AppController,
